2132275be919b3332f85c2b2dc4c7baeab4f6b46
Dyneteve <dyneteve@pixelexperience.org>
2020-02-13 17:44:11 +0000

    ssos: Include our kernel/headers generator
    * Also fix some derps

--- config/common.mk
+++ config/common.mk
@@ -69,5 +69,8 @@ $(call inherit-product, vendor/ssos/config/fonts.mk)
 # Inherit from our versioning
 $(call inherit-product, vendor/ssos/config/versioning.mk)
 
+# Inherit from our kernel/header generator
+$(call inherit-product, vendor/ssos/config/BoardConfigShapeShift.mk)
+
 # Inherit from GMS product config
 $(call inherit-product, vendor/gms/gms_full.mk)
