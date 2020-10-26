55727e2cf2cfa3b72e7dd1fa410cc0ebc58fcfb2
Dyneteve <dyneteve@hentaios.com>
2020-10-26 17:48:57 +0900

    ssos: Import Kai/Sam/Victor fonts for theme picker

--- config/fonts.mk
+++ config/fonts.mk
@@ -73,6 +73,24 @@ PRODUCT_PACKAGES += \
     ZillaSlab-SemiBoldItalic.ttf \
     ZillaSlab-SemiBold.ttf
 
+# Karla family
+PRODUCT_PACKAGES += \
+    Karla-Regular.ttf
+
+# Fraunces family
+PRODUCT_PACKAGES += \
+    Fraunces-SemiBold.ttf
+
+# BigShouldersText family
+PRODUCT_PACKAGES += \
+    BigShouldersText-Bold.ttf \
+    BigShouldersText-ExtraBold.ttf
+
+# Barlow family
+PRODUCT_PACKAGES += \
+    Barlow-Bold.ttf \
+    Barlow-Medium.ttf
+
 # Customization overlays
 PRODUCT_PACKAGES += \
     FontArbutusSourceOverlay \
@@ -86,7 +104,10 @@ PRODUCT_PACKAGES += \
     FontMittelschrift \
     FontPissel \
     FontReemKufi \
-    FontRoboto
+    FontRoboto \
+    FontKaiOverlay \
+    FontSamOverlay \
+    FontVictorOverlay
 
 PRODUCT_COPY_FILES += \
     vendor/ssos/fonts/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml
