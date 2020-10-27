d96efb90ecd9f7128670ca00095b0945e5e4f67e
AshutoshSundresh <ashutoshsundresh@gmail.com>
2020-10-27 15:15:08 +0530

    ssos: Rebrand PixelSetupWizardOverlay

--- config/rro_overlays.mk
+++ config/rro_overlays.mk
@@ -27,4 +27,4 @@ PRODUCT_PACKAGES += \
     IconPackFilledPixelThemePickerOverlay \
     IconPackRoundedPixelLauncherOverlay \
     IconPackRoundedPixelThemePickerOverlay \
-    PixelSetupWizardOverlayDerp
+    PixelSetupWizardOverlaySsos
